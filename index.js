b3c6550971919df57102fedc7fcc1b1e6237f08b
Bailey <r01525004@ntu.edu.tw>
2014-06-03 23:31:41 +0800

    update2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 	//document.getElementById("domainName").innerHTML=window.location.host;
 console.log(window.location.host);
 
-chrome.browserAction.getPopup(function(){
+chrome.browserAction.onClicked.addListener(function(){
 	document.getElementById("domainName").html=window.location.host;//window.location.host;
 	document.write(window.location.host);
 });
